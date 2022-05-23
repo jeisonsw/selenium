92e6f462d13cb15db1e8111f3ebb9c53260b3dec
jeisonsw <welleslay@ukr.net>
2022-05-23 22:39:17 +0300

    add selenium listeners

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as chrome_options
+from selenium.webdriver.support.event_firing_webdriver import EventFiringWebDriver
+
+from abstract.selenium_listener import MyListener
+
+
+@pytest.fixture
+def get_chrome_options():
+    options = chrome_options()
+    options.add_argument('chrome')  # use headless if you do not need browser ui
+    options.add_argument('--start-maximized')
+    options.add_argument('--window-size=1900,1020')
+    return options
+
+
+@pytest.fixture
+def get_webdriver(get_chrome_options):
+    options = get_chrome_options
+    driver = webdriver.Chrome(executable_path='C:/Users/user/Desktop/webdriver/chromedriver.exe', options=options)
+    return driver
+
+
+@pytest.fixture(scope='function')
+def setup(request, get_webdriver):
+    driver = get_webdriver
+    driver = EventFiringWebDriver(driver, MyListener())
+    url = 'https://www.macys.com/'
+    if request.cls is not None:
+        request.cls.driver = driver
+    driver.get(url)
+    yield driver
+    driver.quit()
---
+++ tests/test_homepage.py
@@ -0,0 +1,19 @@
+import time
+
+import pytest
+
+from pom.homepage_nav import HomepageNav
+
+
+@pytest.mark.usefixtures('setup')
+class TestHomepage:
+
+    def test_nav_links(self):
+        homepage_nav = HomepageNav(self.driver)
+        actual_links = homepage_nav.get_nav_links_text()
+        expected_links = homepage_nav.NAV_LINK_TEXT
+        assert expected_links == actual_links, 'Validating nav links text'
+
+        for index in range(12):
+            homepage_nav.get_nav_links()[index].click()
+
